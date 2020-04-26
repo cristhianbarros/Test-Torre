326e41c166063d776ca2903065b62e0297055109
Cristhian Barros <cristhianbarros91@hotmail.com>
2020-04-26 08:44:56 -0500

    Final Changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
          production: true,
          urlSearch: 'https://search.torre.co',
-         urlBackend: 'http://torre.test/api',
+         urlBackend: 'https://cryptic-bastion-10198.herokuapp.com/api',
          urlBio: 'https://torre.bio/api',
          urlBio2: 'https://bio.torre.co/api/people',
        };
